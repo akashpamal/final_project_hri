ae5d1f4f0d73cb536a1077b1ce12b7b41ef915b1
Akash <64038661+akashpamal@users.noreply.github.com>
2025-04-09 20:59:11 -0400

    Add mediapipe coord frame translation + draft of Nao move by coord

---
+++ mediapipe_calculate_joint_poses.py
@@ -0,0 +1,114 @@
+# %%
+import cv2
+import mediapipe as mp
+import csv
+import math
+import pandas as pd
+import json
+import matplotlib.pyplot as plt
+
+"""
+Given a video file and sampling_frequency, finds the landmarks and writes them out to a json file
+sampling_frequency of n means 1 in ever n frames is processed/saved
+
+Returns the json object, also writes it to a file
+"""
+def process_video(video_in_file, json_out_file, sampling_frequency=30):
+    cap = cv2.VideoCapture(video_in_file)
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    frame_count = -1
+    
+    landmarks_json = dict() # timestamp : [landmarks]
+    while cap.isOpened():
+        ret, frame = cap.read()
+        frame_count += 1
+        
+        timestamp = float(frame_count / fps) # timestamp in seconds of this frame
+        
+        if frame_count % sampling_frequency != 0: # only record body poses with sampling_frequency frequency
+            # print('ignoring timestamp:', timestamp, 'params: timestamp', timestamp, 'sampling_frequency', sampling_frequency)
+            # TODO debug this sampling frequency
+            continue
+
+        if not ret: # TODO make this more robust?
+            break
+        
+        # Convert the frame to RGB
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+        # Process the frame with MediaPipe Pose
+        result = pose.process(frame_rgb)
+
+        # Draw the pose landmarks on the frame
+        # if result.pose_landmarks:
+            # mp_drawing.draw_landmarks(frame, result.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+
+        landmarks = dict()
+        for idx, landmark in enumerate(result.pose_world_landmarks.landmark):
+            landmarks[mp_pose.PoseLandmark(idx).name] = [landmark.x, landmark.y, landmark.z]
+        landmarks_json[timestamp] = landmarks
+        # Display the frame
+        # cv2.imshow('MediaPipe Pose', frame)
+
+        # Exit if 'q' keypyt
+    with open(json_out_file, 'w', encoding='utf-8') as json_file:
+        json.dump(landmarks_json, json_file, ensure_ascii=False, indent=4)
+    return landmarks_json
+
+
+# %%
+
+# if __name__ == '__main__':
+# Initialize MediaPipe Pose and Drawing utilities
+mp_pose = mp.solutions.pose
+mp_drawing = mp.solutions.drawing_utils
+pose = mp_pose.Pose()
+
+
+landmarks_json = process_video(video_in_file='./left_wrist_movement.mov', json_out_file='landmarks.json') # also returns the json
+times = sorted(landmarks_json.keys())
+print('Times:', times) # lists each timestamp at which we have saved the body pose. This can be adjusted with the sampling_frequency parameter
+
+# %%
+l_wrist_poses = [landmarks_json[time]['LEFT_WRIST'] for time in times]
+l_wrist_mediapipe_x = [elem[0] for elem in l_wrist_poses]
+l_wrist_mediapipe_y = [elem[1] for elem in l_wrist_poses]
+l_wrist_mediapipe_z = [elem[2] for elem in l_wrist_poses]
+
+scale_factor = 1/3
+l_wrist_nao_z = [-elem * scale_factor for elem in l_wrist_mediapipe_x]
+l_wrist_nao_y = [elem * scale_factor for elem in l_wrist_mediapipe_y]
+l_wrist_nao_x = [elem * scale_factor for elem in l_wrist_mediapipe_z]
+
+with open("nao_coords.csv", "w") as f:
+    writer = csv.writer(f)
+    writer.writerow(["Time", "LarmX", "LarmY", "LArmZ"])
+    for i in range(len(times)):
+        row = [times[i], l_wrist_nao_x[i], l_wrist_nao_y[i], l_wrist_nao_z[i]]
+        writer.writerow(row)
+
+plt.plot(times, l_wrist_mediapipe_x, label='Left Wrist X')
+plt.plot(times, l_wrist_mediapipe_y, label='Left Wrist Y')
+plt.plot(times, l_wrist_mediapipe_z, label='Left Wrist Z')
+plt.xlabel('Time (s)')
+plt.ylabel('Left Wrist Position (m)')
+plt.title('Left Wrist Position vs Time')
+plt.legend()
+plt.grid(True)
+plt.show()
+
+"""
+Mediapipe axes:
+X axis points down
+Y axis to the left
+Z axis points forward
+"""
+
+"""
+Nao axes:
+Z axis points up
+Y axis points to the left
+X axis points forward
+"""
+
+# Assume Nao is about 1/3rd of the height of a human
---
+++ move_nao_by_coord.py
@@ -0,0 +1,113 @@
+# %%
+'''Cartesian control: Arm trajectory example'''
+
+import sys
+import motion
+import almath
+from naoqi import ALProxy
+import codecs
+import csv
+
+
+def StiffnessOn(proxy):
+  #We use the "Body" name to signify the collection of all joints
+  pNames = "Body"
+  pStiffnessLists = 1.0
+  pTimeLists = 1.0
+  proxy.stiffnessInterpolation(pNames, pStiffnessLists, pTimeLists)
+
+# %%
+def main(robotIP):
+    ''' Example showing a path of two positions
+    Warning: Needs a PoseInit before executing
+    '''
+
+    # Init proxies.
+    try:
+        motionProxy = ALProxy("ALMotion", robotIP, 9559)
+    except Exception, e:
+        print "Could not create proxy to ALMotion"
+        print "Error was: ", e
+
+    try:
+        postureProxy = ALProxy("ALRobotPosture", robotIP, 9559)
+    except Exception, e:
+        print "Could not create proxy to ALRobotPosture"
+        print "Error was: ", e
+
+    # Set NAO in Stiffness On
+    StiffnessOn(motionProxy)
+
+    # Send NAO to Pose Init
+    # postureProxy.goToPosture("StandInit", 0.5)
+
+    effector   = "LArm"
+    space      = motion.FRAME_TORSO
+    axisMask   = almath.AXIS_MASK_VEL    # just control position
+    isAbsolute = True
+    
+    times = []
+    coordinates = []
+
+    # Open and read the CSV file
+    with open('nao_coords.csv', 'r') as csvfile:
+        reader = csv.reader(csvfile)
+        header = next(reader)  # Skip the header row
+
+        # Extract data from each row
+        for row in reader:
+            times.append(float(row[0]))  # Add time value to times list
+            coordinates.append([float(row[1]), float(row[2]), float(row[3])])  # Add x, y, z coordinates to coordinates list
+    coordinates = [coordinate + [0,0,0] for coordinate in coordinates] # append empty values for the rotation in XYZ axis
+    times = [elem * 3 for elem in times] # slow down the movements by some factor
+    times = times[:1]
+    coordinates = coordinates[:1]
+    print('times:', times)
+    print('coordinates:', coordinates)
+    # motionProxy.positionInterpolation(effector, space, coordinates, axisMask, times, isAbsolute)
+    motionProxy.positionInterpolation(effector, space, coordinates, axisMask, times, isAbsolute)
+    
+    # # Since we are in relative, the current position is zero
+    # currentPos = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+
+    # # Define the changes relative to the current position
+    # dx         =  0.00      # translation axis X (meters)
+    # dy         =  0.13     # translation axis Y (meters)
+    # dz         =  0.26      # translation axis Z (meters)
+    # dwx        =  -1.22      # rotation axis X (radians)
+    # dwy        =  0.52      # rotation axis Y (radians)
+    # dwz        =  0.01     # rotation axis Z (radians)
+    
+    # # dx         =  0.12336114048957825      # translation axis X (meters)
+    # # dy         =  0.1323665827512741      # translation axis Y (meters)
+    # # dz         =  0.26347923278808594      # translation axis Z (meters)
+    # # dwx        =  -1.21964430809021      # rotation axis X (radians)
+    # # dwy        =  0.5219550132751465      # rotation axis Y (radians)
+    # # dwz        =  0.01353203784674406      # rotation axis Z (radians)
+    # targetPos  = [dx, dy, dz, dwx, dwy, dwz]
+    
+    pos1 = [-0.05083545049031575, -0.11260572075843811, 0.039958963791529335, 0, 0, 0]
+    # pos1 = [0.14344285428524017, 0.13446059823036194, 0.5971754789352417, -1.6460217237472534, -0.9850037693977356, 0.2681563198566437]
+    # pos2 = [0.08367110043764114, 0.09527045488357544, 0.21045847237110138, -1.5316928625106812, 1.2437962293624878, -0.1373995989561081]
+
+    # Go to the target and back again
+    # path       = [targetPos, currentPos]
+    path       = [pos1]
+    times      = [3.0] # seconds
+
+    # currPos = motionProxy.getPosition(effector, space, isAbsolute)
+    # print currPos
+    
+    motionProxy.positionInterpolation(effector, space, path, axisMask, times, isAbsolute)
+
+
+if __name__ == "__main__":
+    robotIp = "192.168.1.30"
+    # robotIp = "172.0.0.1"
+
+    if len(sys.argv) <= 1:
+        print "Usage python motion_cartesianArm1.py robotIP (optional default: 127.0.0.1)"
+    else:
+        robotIp = sys.argv[1]
+
+    main(robotIp)
